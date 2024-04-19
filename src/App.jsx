2e43f200d5b30d518ca133ba5cd1b0a4a4d74675
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-19 22:21:24 +0530

    Implement AppLayout with fetching Menu data Implement Routes with error handling for Menu unknown address navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 // The data loading/submit capabilities using Router enabled with new ReactRouter 6.4
 // Use createBrowserRouter function and pass array of objects, each object is a Route
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  // Layout route no need any path, only purpose to provide layout to the application
+  // Since this doesnt have a path, its technically called in React-router, a layout route
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
 ]);
 
 function App() {
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// It is a convention to have loader function in same component that we need the data
+// Then export it into route and get the data from route using a custom hook when component loads [Render as you fetch]
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
